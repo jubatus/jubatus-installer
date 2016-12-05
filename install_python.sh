b2d6fb1195f913772555acfc6551591f707617b2
IMAMASU Ryohei <imms.ryohei@gmail.com>
2016-12-05 13:24:36 +0900

    use pushd and popd instead of cd

--- install_python.sh
+++ install_python.sh
@@ -5,19 +5,19 @@ PREFIX="${HOME}/local"
 
 
   mkdir download
-  cd download
+  pushd download
 
   wget http://www.python.org/ftp/python/${PYTHON_VER}/Python-${PYTHON_VER}.tgz
   wget http://peak.telecommunity.com/dist/ez_setup.py
 
   tar zxf Python-${PYTHON_VER}.tgz
 
-  cd Python-${PYTHON_VER}
+  pushd Python-${PYTHON_VER}
   ./configure --prefix=${PREFIX}
   make
   make install
+  popd
 
-  cd ..
   export PATH=${PREFIX}/bin/:$PATH
   python ez_setup.py
   easy_install pip
